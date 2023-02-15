9225c8cdc3c3275d193775cc6bc37d20a96a01d4
WojciechLiszka <wliszka99@gmail.com>
2023-02-15 17:30:35 +0100

    added HttpConntentHelper

---
+++ Library_Api.Test/Helpers/HttpContentHelper.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Api.Test.Helpers
+{
+    public static class HttpContentHelper
+    {
+        
+            public static HttpContent ToJsonHttpContent(this object obj)
+            {
+                var json = JsonConvert.SerializeObject(obj);
+                var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+                return httpContent;
+            }
+        
+    }
+}
